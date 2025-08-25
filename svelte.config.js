e0a4671f61d2481714ac13d5379fb7b77f7cb2a6
AJM-L <144962191+AJM-L@users.noreply.github.com>
2025-08-25 23:56:53 +0200

    Fix back button routing: correct base path from /cwf-website to /cwf-site and update BackButton component

--- svelte.config.js
+++ svelte.config.js
@@ -14,7 +14,7 @@ const config = {
 			strict: false
 		}),
 		paths: {
-			base: process.env.NODE_ENV === 'production' ? '/cwf-website' : ''
+			base: process.env.NODE_ENV === 'production' ? '/cwf-site' : ''
 		}
 	}
 };
